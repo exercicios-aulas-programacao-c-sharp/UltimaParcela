5ce9de2252bc690f04e46b5b16c508c1ef3ebc08
diego_neri <diego@diegoneri.com>
2020-06-05 22:29:03 -0300

    Cria o programa UltimaParcela 1.0
    Finaliza a codificação do programa

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroParcelas;
+            decimal valorTotal;
+            decimal valorParcela;
+            DateTime dataUltimaParcela;
+
+            Console.Write("Valor total do financiamento................: ");
+            valorTotal = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Quantidade de parcelas mansais (sem juros)..: ");
+            numeroParcelas = Convert.ToInt32(Console.ReadLine());
+
+            valorParcela = valorTotal / numeroParcelas;
+            dataUltimaParcela = DateTime.Now.AddMonths(numeroParcelas);
+            
+            Console.WriteLine($"Sua última parcela será em {dataUltimaParcela.ToShortDateString()}, no valor de {valorParcela:C2}.");
+
+        }
+    }
+}
